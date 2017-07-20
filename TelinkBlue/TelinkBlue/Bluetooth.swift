8be98d86c4fed17f57e8f995ba1a397e08c19935
Arvinky <otw0909@gmail.com>
2017-07-20 18:30:57 +0800

    Add Something

--- TelinkBlue/TelinkBlue/Bluetooth.swift
+++ TelinkBlue/TelinkBlue/Bluetooth.swift
@@ -8,21 +8,34 @@
 
 import Foundation
 import CoreBluetooth
+let AdvDataManafacturerData = "kCBAdvDataManufacturerData"
+let AdvDataConnectedAble = "kCBAdvDataIsConnectable"
+let AdvDataLocalName = "kCBAdvDataLocalName"
+let AdvDataServiceUUIDs = "kCBAdvDataServiceUUIDs"
+
 
 public class Device: NSObject {
     var peripheral : CBPeripheral?
-    var sevices : [CBMutableService]?
+    var sevices : [CBMutableService] = [CBMutableService].init()
     var state : PeripheralState?
     var beenConnect : Bool?
-    var advertisement : NSMutableDictionary?
+    var advertisement : NSMutableDictionary = NSMutableDictionary.init()
     var rssi : Any?
-    override init() {
-        sevices = Array.init()
-        advertisement = NSMutableDictionary.init()
+    var name = "Unknow"
+    var adveDataSevices : String {
+        for str in advertisement.allKeys {
+            if (str as! String).compare(AdvDataServiceUUIDs)==ComparisonResult.orderedSame {
+                if advertisement[str] is Array<Any> {
+                    let s = advertisement.value(forKey: str as! String) as! Array<Any>
+                    return "\(s.count) Services"
+                }
+            }
+        }
+        return "NoServices"
     }
 }
 @objc public enum PeripheralState :Int {
-    case PeripheralStateDiscovered = 1, PeripheralStateUpdateRSSI, PeripheralStateDidConnect
+    case PeripheralStateDiscovered = 1, PeripheralStateUpdateRSSI, PeripheralStateDidConnect, PeripheralStateFinishDiscoverATT,PeripheralStateDiscoverATTTimout
 }
 @objc public protocol BluetoothHandleProtocol : NSObjectProtocol {
     func updateBluetoothState(_ state : CBManagerState)
@@ -33,35 +46,38 @@ extension CBPeripheral {
         return self.identifier.uuidString
     }
 }
+public let disconverATTTime : TimeInterval = 10
+
 class BluetoothHandle: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
     
     var manager : CBCentralManager?
     var delegate : BluetoothHandleProtocol?
-    var devicesDiscovered : [Device]?
+    var devicesDiscovered = [Device]()
     var currentDevice : Device?
-    
+    var descriptors = [CBDescriptor]()
+    var disconverATTtimer : Timer?
     
     static let handle = BluetoothHandle.init()
     static func startScan() { BluetoothHandle.handle.manager?.scanForPeripherals(withServices: nil, options: nil) }
     static func stopScan() { BluetoothHandle.handle.manager?.stopScan() }
     
     private override init() {
-        let quene = DispatchQueue.init(label: "BluetoothQuene")
-        manager = CBCentralManager.init(delegate: nil, queue: quene)
+        _ = DispatchQueue.init(label: "BluetoothQuene")
+        manager = CBCentralManager.init(delegate: nil, queue: DispatchQueue.main)
     }
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         delegate?.updateBluetoothState(central.state)
     }
     func whetherContainObj(_ source : [Device], _ peripheral : CBPeripheral) -> Any {
         for device in source {
-            if device.peripheral?.identifyString.compare((device.peripheral?.identifyString)!)==ComparisonResult.orderedSame {
+            if device.peripheral?.identifyString.compare(peripheral.identifyString)==ComparisonResult.orderedSame {
                 return device;
             }
         }
         return NSNotFound
     }
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        let obj = self.whetherContainObj(devicesDiscovered!, peripheral)
+        let obj = self.whetherContainObj(devicesDiscovered, peripheral)
         var device : Device?
         if obj is Device {
             device = obj as? Device
@@ -69,29 +85,73 @@ class BluetoothHandle: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate 
             device?.state = PeripheralState.PeripheralStateUpdateRSSI
         }else{
             device = Device()
+            device?.peripheral = peripheral
             device?.rssi = RSSI
-            device?.advertisement = NSMutableDictionary.init(dictionary: advertisementData)
+            if advertisementData.count > 2 {
+                device?.name = (advertisementData[AdvDataLocalName] as! String)
+                device?.advertisement.addEntries(from: advertisementData)
+            }else if (peripheral.name != nil) {
+                device?.name = peripheral.name!
+            }
+            
             device?.state = PeripheralState.PeripheralStateDiscovered
-            devicesDiscovered?.append(device!)
+            devicesDiscovered.append(device!)
             delegate?.updatePeripheralState!((device?.state)!, device!)
         }
         print("\(advertisementData)\n")
     }
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        let device = whetherContainObj(devicesDiscovered!, peripheral)
+        let device = whetherContainObj(devicesDiscovered, peripheral)
         let obj = device as! Device
         obj.state = PeripheralState.PeripheralStateDidConnect
         delegate?.updatePeripheralState!(obj.state!, obj)
         self.currentDevice = obj
         peripheral.delegate = self
         peripheral.discoverServices(nil)
+        
+        disconverATTtimer = Timer.scheduledTimer(timeInterval: disconverATTTime, target: self, selector: #selector(BluetoothHandle.discoverATTTimeout), userInfo: nil, repeats: true)
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         for service in peripheral.services! {
             let mutSerivce = CBMutableService.init(type: service.uuid, primary: true)
-            self.currentDevice?.sevices?.append(mutSerivce)
+            self.currentDevice?.sevices.append(mutSerivce)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+        descriptors.removeAll()
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        for mutser in (self.currentDevice?.sevices)! {
+            if mutser.uuid.uuidString.compare(service.uuid.uuidString) == ComparisonResult.orderedSame {
+                mutser.characteristics = NSArray.init(array: service.characteristics!) as? [CBCharacteristic]
+//                mutser.characteristics = service.characteristics
+            }
+        }
+        for cha in service.characteristics! {
+            peripheral.readValue(for: cha)
+            peripheral.discoverDescriptors(for: cha)
+        }
+        
+    }
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
+        for des in characteristic.descriptors! {
+            peripheral.readValue(for: des)
+            descriptors.append(des)
+        }
+    }
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor descriptor: CBDescriptor, error: Error?) {
+        if descriptors.contains(descriptor) {
+            descriptors.remove(at: descriptors.index(of: descriptor)!)
+        }
+        if descriptors.count < 1 {
+            self.currentDevice?.state = PeripheralState.PeripheralStateFinishDiscoverATT
+            delegate?.updatePeripheralState!(PeripheralState.PeripheralStateFinishDiscoverATT, currentDevice!)
         }
     }
     
+    func discoverATTTimeout() {
+        self.currentDevice?.state = PeripheralState.PeripheralStateDiscoverATTTimout
+        delegate?.updatePeripheralState!(PeripheralState.PeripheralStateDiscoverATTTimout, currentDevice!)
+    }
 }
--- TelinkBlue/TelinkBlue/ViewController.swift
+++ TelinkBlue/TelinkBlue/ViewController.swift
@@ -8,20 +8,59 @@
 
 import UIKit
 import CoreBluetooth
-class ViewController: UIViewController, BluetoothHandleProtocol {
-    func updatePeripheralState(_ state: PeripheralState, _ device: Device) {
-        
+public class DeviceCell : UITableViewCell {
+    @IBOutlet weak var rssiImg: UIImageView!
+    @IBOutlet weak var rssiLab: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var service: UILabel!
+}
+class BasicVC : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        normalSetting()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setBlock()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        initBlock()
+    }
+    func normalSetting() { }
+    func setBlock() { }
+    func initBlock() { }
+}
+class ViewController: BasicVC, BluetoothHandleProtocol , UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         BluetoothHandle.handle.manager?.delegate = BluetoothHandle.handle
         BluetoothHandle.startScan()
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    func updateData(_ freshcontrol : UIRefreshControl) {
+        BluetoothHandle.handle.devicesDiscovered.removeAll()
+        tableView.reloadData()
+        BluetoothHandle.startScan()
+        freshcontrol.endRefreshing()
+    }
+    override func normalSetting() {
+        let freshControl = UIRefreshControl.init()
+        freshControl.addTarget(self, action: #selector(updateData), for: UIControlEvents.valueChanged)
+        tableView.addSubview(freshControl)
+        tableView.estimatedRowHeight = 20
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+    override func setBlock() {
         BluetoothHandle.handle.delegate = self
     }
+    override func initBlock() {
+        BluetoothHandle.handle.delegate = nil
+    }
+    
     func updateBluetoothState(_ state: CBManagerState) {
         switch state {
         case .poweredOn:
@@ -35,6 +74,40 @@ class ViewController: UIViewController, BluetoothHandleProtocol {
         default: break
         }
     }
-    
+    func delayDismiss(_ alert : UIAlertController) {
+        alert.dismiss(animated: true, completion: nil)
+    }
+    func updatePeripheralState(_ state: PeripheralState, _ device: Device) {
+        let index = BluetoothHandle.handle.devicesDiscovered.index(of: device)
+        let indexPath = IndexPath.init(row: index!, section: 0)
+        
+        switch state {
+        case .PeripheralStateUpdateRSSI:
+            tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            break
+        case .PeripheralStateDiscovered:
+            tableView.insertRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            tableView.reloadData()
+        case .PeripheralStateDiscoverATTTimout:
+            let alert = UIAlertController.init(title: "tips", message: "discover time out", preferredStyle: UIAlertControllerStyle.alert)
+            self.navigationController?.present(alert, animated: true, completion: nil)
+            self.perform(#selector(delayDismiss(_:)), with: alert, afterDelay: 2)
+            
+        default:
+            print("other\(state)")
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return BluetoothHandle.handle.devicesDiscovered.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DeviceCell") as! DeviceCell
+        let  device = BluetoothHandle.handle.devicesDiscovered[indexPath.row]
+        
+        cell.name.text = device.name
+        cell.service.text = device.adveDataSevices
+        return cell
+    }
 }
 
